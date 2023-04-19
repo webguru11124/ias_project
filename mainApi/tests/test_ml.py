c5d183fc85ede3c3f8c6c1f9e70aa49d0fa8386d
james <klegendary7811@gmail.com>
2023-04-19 03:47:44 +0200

    add test ml

---
+++ mainApi/tests/test_ml.py
@@ -0,0 +1,27 @@
+import pytest
+import os
+import subprocess
+
+
+class TestML:
+
+    INPUT_FILE_PATH = '/app/mainApi/tests/test_image_tiles/aaa.tif'
+    OUTPUT_FILE_PATH = '/app/mainApi/tests/ml_out/aaa.tif'
+    OUTPUT_FOLDER = '/app/mainApi/tests/ml_out'
+
+    @classmethod
+    def setup_class(cls):
+        if not os.path.exists(cls.OUTPUT_FOLDER):
+            os.makedirs(cls.OUTPUT_FOLDER)
+
+    @pytest.mark.asyncio
+    async def test_basic(self):
+        cmd_str = "wine /app/mainApi/ml_lib/lung/grid_analize.exe '{inputPath}' '{outputPath}' src_param.txt".format(
+            inputPath=self.INPUT_FILE_PATH, outputPath=self.OUTPUT_FILE_PATH
+        )
+        subprocess.call(cmd_str, shell=True)
+
+
+
+
+

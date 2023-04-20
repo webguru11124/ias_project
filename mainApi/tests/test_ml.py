1e0a6c7f88d09ce36d61298115f283f2e46f9a75
james <klegendary7811@gmail.com>
2023-04-20 11:54:35 +0200

    update tests

--- mainApi/tests/test_ml.py
+++ mainApi/tests/test_ml.py
@@ -22,7 +22,7 @@ class TestML:
         subprocess.call(cmd_str, shell=True)
 
         cmd_str = "cp '{inputPath}' '{outputPath}'".format(
-            inputPath=self.WINE_OUTPUT_FOLDER + '/*', outputPath=self.TEST_OUTPUT_FOLDER + '/*'
+            inputPath=self.WINE_OUTPUT_FOLDER + '/*.*', outputPath=self.TEST_OUTPUT_FOLDER + '/'
         )
         subprocess.call(cmd_str, shell=True)
 

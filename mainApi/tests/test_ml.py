87db87c2d2cb0d40451cd45e7179cabfc5f16fdf
james <klegendary7811@gmail.com>
2023-04-20 11:40:25 +0200

    update tests

--- mainApi/tests/test_ml.py
+++ mainApi/tests/test_ml.py
@@ -6,19 +6,30 @@ import subprocess
 class TestML:
 
     INPUT_FILE_PATH = '/app/mainApi/tests/test_image_tiles/aaa.tif'
-    OUTPUT_FILE_PATH = '/app/mainApi/tests/ml_out/aaa.tif'
-    OUTPUT_FOLDER = '/app/mainApi/tests/ml_out'
+    OUTPUT_FILE_PATH = 'aaa.jpg'
+    WINE_OUTPUT_FOLDER = '/home/wine/ml_out'
+    TEST_OUTPUT_FOLDER = '/app/mainApi/tests/ml_out'
 
     @classmethod
     def setup_class(cls):
-        if not os.path.exists(cls.OUTPUT_FOLDER):
-            os.makedirs(cls.OUTPUT_FOLDER)
+        if not os.path.exists(cls.WINE_OUTPUT_FOLDER):
+            os.makedirs(cls.WINE_OUTPUT_FOLDER)
+
+        if not os.path.exists(cls.TEST_OUTPUT_FOLDER):
+            os.makedirs(cls.TEST_OUTPUT_FOLDER)
 
     def test_basic(self):
         cmd_str = "su -p -l wine -c 'wine /app/mainApi/ml_lib/lung/grid_analize.exe {inputPath} {outputPath} /app/mainApi/ml_lib/lung/src_param.txt'".format(
             inputPath=self.INPUT_FILE_PATH, outputPath=self.OUTPUT_FILE_PATH
         )
         subprocess.call(cmd_str, shell=True)
+
+        cmd_str = "cp '{inputPath}' '{outputPath}'".format(
+            inputPath=self.WINE_OUTPUT_FOLDER + '/*', outputPath=self.TEST_OUTPUT_FOLDER + '/*'
+        )
+        subprocess.call(cmd_str, shell=True)
+
+
 
 
 

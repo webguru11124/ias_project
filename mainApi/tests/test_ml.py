4aec87c266b7f614de1acbff0be4538554efa8d9
james <klegendary7811@gmail.com>
2023-04-21 02:58:56 +0200

    update tests

--- mainApi/tests/test_ml.py
+++ mainApi/tests/test_ml.py
@@ -7,27 +7,21 @@ class TestML:
 
     INPUT_FILE_PATH = '/app/mainApi/tests/test_image_tiles/aaa.tif'
     OUTPUT_FILE_PATH = 'aaa.jpg'
-    WINE_OUTPUT_FOLDER = '/home/wine'
+    WINE_OUTPUT_FOLDER = '/home/wine/ml_temp'
     TEST_OUTPUT_FOLDER = '/app/mainApi/tests/ml_out'
-
-    @classmethod
-    def setup_class(cls):
-        if not os.path.exists(cls.TEST_OUTPUT_FOLDER):
-            os.makedirs(cls.TEST_OUTPUT_FOLDER)
 
     @pytest.mark.asyncio
     async def test_basic(self):
+
+        cmd_str = "su -c 'mkdir {folderPath}' wine".format(folderPath=self.WINE_OUTPUT_FOLDER)
+        await asyncio.create_subprocess_shell(cmd_str)
+
         cmd_str = "su -p -l wine -c 'wine /app/mainApi/ml_lib/lung/grid_analize.exe {inputPath} {outputPath} /app/mainApi/ml_lib/lung/src_param.txt'".format(
             inputPath=self.INPUT_FILE_PATH, outputPath=self.OUTPUT_FILE_PATH
         )
         # subprocess.call(cmd_str, shell=True)
         await asyncio.create_subprocess_shell(cmd_str)
 
-        cmd_str = "cp {inputPath} {outputPath}".format(
-            inputPath=self.WINE_OUTPUT_FOLDER + '/*.*', outputPath=self.TEST_OUTPUT_FOLDER + '/'
-        )
-        # subprocess.call(cmd_str, shell=True)
-        await asyncio.create_subprocess_shell(cmd_str)
 
 
 

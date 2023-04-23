916688c9a1fe7ce1dc5fdb9e9d23f3e7368d2b37
Oleksandr <alexandar1992011@outlook.com>
2023-04-23 07:59:54 -0700

    Add Group button in TabGroup.

--- react/src/components/tabsLeft/contents/file/PositionDialog/tabs/TabGroup.jsx
+++ react/src/components/tabsLeft/contents/file/PositionDialog/tabs/TabGroup.jsx
@@ -1,7 +1,9 @@
 import DialogContent from '@/components/mui/DialogContent';
 import useTilingStore from '@/stores/useTilingStore';
 import {
+  Box,
   Button,
+  CircularProgress,
   DialogActions,
   FormControl,
   Grid,
@@ -15,16 +17,72 @@ import {
 import { useEffect, useMemo, useState } from 'react';
 import store from '@/reducers';
 import { connect } from 'react-redux';
+import DataTable from '@/components/mui/DataTable';
+import BoxCenter from '@/components/mui/BoxCenter';
+import { List } from 'semantic-ui-react';
+import { Typography } from 'react-md';
 
 const TabGroup = (props) => {
+  const { tiles, loadTiles } = useTilingStore();
+
+  const [loading, setLoading] = useState(true);
+
+  const [groupList, setGroupList] = useState([]);
+
   useEffect(() => {
-    //console.log(props.content)
-  }, []);
+    //console.log(tiles);
+    if (tiles != [] || tiles != null) setLoading(false);
+  }, [tiles]);
+
+  const handleNew = async () => {};
+
+  const handleSave = async () => {};
 
   return (
-    <div>
-      <p>this is the TabGroup field</p>
-    </div>
+    <>
+      <DialogContent dividers sx={{ height: '100%' }}>
+        {loading ? (
+          <BoxCenter height="100%">
+            <CircularProgress />
+          </BoxCenter>
+        ) : (
+          <Grid container sx={{ height: '100%' }}>
+            <Grid
+              item
+              container
+              xl={2}
+              lg={3}
+              xs={4}
+              sx={{ p: 2, height: 'fit-content' }}
+              spacing={2}
+            >
+              <Grid item xs={12}>
+                <FormControl>
+                  <Typography mr={4}>Groups:</Typography>
+                  <List></List>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleNew}
+                  >
+                    New
+                  </Button>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Grid item xl={10} lg={9} xs={8} sx={{ height: '100%' }}></Grid>
+          </Grid>
+        )}
+      </DialogContent>
+      <DialogActions sx={{ px: 3, py: 2 }}>
+        <Button variant="contained" color="primary" onClick={handleSave}>
+          Save
+        </Button>
+        <Button variant="outlined" color="warning">
+          Cancel
+        </Button>
+      </DialogActions>
+    </>
   );
 };
 
@@ -33,196 +91,3 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps)(TabGroup);
-
-/*
-export default function TabGroup() {
-  const { tiles } = useTilingStore();
-  const [align, setAlign] = useState(Alignments.raster);
-  const [dir, setDir] = useState(Directions.horizontal);
-  const dims = useMemo(() => getAvailableDimensions(tiles.length), [tiles]);
-  const [dim, setDim] = useState(dims?.[0] || [0, 0]);
-  const [building, setBuilding] = useState(false);
-
-  const sorted = useMemo(
-    () => tiles.sort((a, b) => a.series - b.series),
-    [tiles],
-  );
-
-  const tilesAligned = useMemo(() => {
-    if (!dim) {
-      return sorted;
-    }
-    const cols = dim[1];
-    const rows = dim[0];
-    // Split the array into sub-arrays of cols
-    let chunks = [];
-
-    // if the direction is horizontal
-    if (dir === Directions.horizontal) {
-      for (let i = 0; i < sorted.length; i += cols) {
-        chunks.push(sorted.slice(i, i + cols));
-      }
-
-      // Reverse every second sub-array for snake layout
-      if (align === Alignments.snake) {
-        for (let i = 1; i < chunks.length; i += 2) {
-          chunks[i].reverse();
-        }
-      }
-    } // if the direction is vertical
-    else if (dir === Directions.vertical) {
-      for (let i = 0; i < rows; i++) chunks.push([]);
-      for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-          const id = j * rows + i;
-          chunks[i].push(sorted.at(j * rows + i));
-        }
-      }
-
-      if (align === Alignments.snake) {
-        let temp = chunks;
-        chunks = [];
-        for (let i = 0; i < rows; i++) chunks.push([]);
-        for (let i = 0; i < rows; i++) {
-          for (let j = 0; j < cols; j++) {
-            //Reverse every second cols for snake layout
-            if (j % 2 == 1) {
-              chunks[i][j] = temp[rows - i - 1][j];
-            } else chunks[i][j] = temp[i][j];
-          }
-        }
-      }
-    }
-
-    // Join the sub-arrays back together
-    const result = [].concat(...chunks);
-
-    return result;
-  }, [sorted, align, dir, dim]);
-
-  const handleBuild = async () => {
-    const [height, width] = dim;
-    const ashlarParams = {
-      width,
-      height,
-      layout: align,
-      direction: dir,
-    };
-
-    setBuilding(true);
-    const output = await buildPyramid(ashlarParams);
-
-    store.dispatch({ type: 'set_image_path_for_avivator', content: output });
-
-    setBuilding(false);
-  };
-   
-  return (
-    <>
-      <DialogContent dividers sx={{ height: '100%' }} loading={building}>
-        <Grid container sx={{ height: '100%' }}>
-          <Grid
-            item
-            container
-            xl={2}
-            lg={3}
-            xs={4}
-            sx={{ p: 2, height: 'fit-content' }}
-            spacing={2}
-          >
-            <Grid item xs={12}>
-              <FormControl fullWidth>
-                <InputLabel>Alignment</InputLabel>
-                <Select
-                  color="primary"
-                  label="Alignment"
-                  size="small"
-                  value={align}
-                  onChange={(e) => setAlign(e.target.value)}
-                >
-                  {Object.values(Alignments).map((align) => (
-                    <MenuItem value={align} key={align}>
-                      {AlignmentLabels[align]}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl fullWidth>
-                <InputLabel>Direction</InputLabel>
-                <Select
-                  color="primary"
-                  label="Direction"
-                  size="small"
-                  value={dir}
-                  onChange={(e) => setDir(e.target.value)}
-                >
-                  {Object.values(Directions).map((dir) => (
-                    <MenuItem value={dir} key={dir}>
-                      {DirectionLabels[dir]}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl fullWidth>
-                <InputLabel>Rows and Columns</InputLabel>
-                <Select
-                  color="primary"
-                  label="Rows and Columns"
-                  size="small"
-                  value={dim.toString()}
-                  onChange={(e) =>
-                    setDim(e.target.value.split(',').map((v) => Number(v)))
-                  }
-                >
-                  {dims.map((dim) => (
-                    <MenuItem value={dim.toString()} key={dim.toString()}>
-                      {`Rows: ${dim[0]}, Cols: ${dim[1]}`}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-          </Grid>
-          <Grid item xl={10} lg={9} xs={8} sx={{ height: '100%' }}>
-            <Paper variant="outlined" sx={{ height: '100%' }}>
-              <TransformWrapper minScale={0.1}>
-                <TransformComponent
-                  wrapperStyle={{ width: '100%', height: '100%' }}
-                >
-                  <ImageList
-                    cols={dim[1]}
-                    gap={0}
-                    sx={{ mb: 0, pointerEvents: 'none', userSelect: 'none' }}
-                  >
-                    {tilesAligned.map(({ _id, thumbnail, filename }) => (
-                      <ImageListItem key={_id}>
-                        <img
-                          src={thumbnail}
-                          alt={filename}
-                          style={{ width: 100, height: 'auto' }}
-                        />
-                      </ImageListItem>
-                    ))}
-                  </ImageList>
-                </TransformComponent>
-              </TransformWrapper>
-            </Paper>
-          </Grid>
-        </Grid>
-      </DialogContent>
-      <DialogActions sx={{ px: 3, py: 2 }}>
-        <Button variant="contained" color="primary" onClick={handleBuild}>
-          Build
-        </Button>
-        <Button variant="outlined" color="warning">
-          Cancel
-        </Button>
-      </DialogActions>
-    </>
-  );
-}
-*/

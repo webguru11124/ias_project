ecca116ca956ddc679b727826673385ec30a2c87
james <klegendary7811@gmail.com>
2023-05-15 04:51:31 +0200

    update ml_lib

--- mainApi/app/images/routers.py
+++ mainApi/app/images/routers.py
@@ -94,7 +94,7 @@ async def mlIPSProcess(request: Request, current_user: UserModelDB = Depends(get
     fileName = imagePath.split("/")[len(imagePath.split("/")) - 1]
     tempPath = tempfile.mkdtemp()
     OUT_PUT_FOLDER = tempPath.split("/")[len(tempPath.split("/")) - 1]
-    OUT_PUT_PATH = '/app/mainApi/app/static/ml_out/' + OUT_PUT_FOLDER
+    OUT_PUT_PATH = 'mainApi/app/static/ml_out/' + OUT_PUT_FOLDER
 
     if not os.path.exists(OUT_PUT_PATH):
         os.makedirs(OUT_PUT_PATH)
@@ -111,6 +111,7 @@ async def mlIPSProcess(request: Request, current_user: UserModelDB = Depends(get
 
     cmd_str += " " + sensitivity
     cmd_str = cmd_str.format(inputPath=imagePath, outputPath=OUT_PUT_PATH + "/" + fileName)
+    print('----->', cmd_str)
     subprocess.call(cmd_str, shell=True)
     return JSONResponse({"success": "success", "image_path": OUT_PUT_PATH + "/" + fileName})
 

49804b6f1a646516e84000391352730649a6d5c1
Oleksandr <alexandar1992011@outlook.com>
2023-06-12 09:05:08 -0800

    Fixed Aligned in Tiling Part

--- react/src/components/tabsLeft/contents/file/PositionDialog/tabs/TabTilingPart.jsx
+++ react/src/components/tabsLeft/contents/file/PositionDialog/tabs/TabTilingPart.jsx
@@ -31,8 +31,8 @@ import Avivator from '../../../../../avivator/Avivator';
 import { Alignments, Directions, SortOrder } from './constants';
 import { buildPyramid } from '@/api/tiling';
 import { Typography } from 'react-md';
-import { getMetadataFromDataBase } from '@/api/tiles';
-import { cleanUrl } from '@/helpers/file';
+//import { getMetadataFromDataBase } from '@/api/tiles';
+//import { cleanUrl } from '@/helpers/file';
 
 const tilingMenus = [
   'Edit',
@@ -106,6 +106,7 @@ const TabTiling = (props) => {
 
   const selectedImage = useSelector((state) => state.selectedImage);
 
+  /*
   //Get the *.xml url from the original Image file name
   const getXmlUrl = (url, filename) => {
     const realUrl = url.split(filename)[0];
@@ -115,6 +116,7 @@ const TabTiling = (props) => {
     const newUrl = realUrl + name + newExtension;
     return newUrl;
   };
+  */
 
   //Get the image of ome tiff file extension from the original url
   const getOmeTiffUrl = (url) => {
@@ -237,10 +239,6 @@ const TabTiling = (props) => {
     if (!tiles || tiles.length === 0) return;
 
     if (tiles) {
-      setDim([1, tiles.length]);
-      if (tiles[0]) {
-        setResultImagePath(getOmeTiffUrl(tiles[0].url));
-      }
       setInfoMessage(`${tiles.length} images are loaded.`);
 
       const sortedTiles = tiles.sort((a, b) =>
@@ -357,6 +355,9 @@ const TabTiling = (props) => {
         }
       }
     });
+
+    setAlignCol(1);
+    setAlignRow(res.length);
 
     return res;
   };
@@ -638,26 +639,26 @@ const TabTiling = (props) => {
   }, [tiles]);
 
   // return tiles aligned in alignment function
-  const tilesAligned = useMemo(() => {
+  const alignedImageList = useMemo(() => {
     let sortedTiles;
-    if (!tiles || tiles.length === 0) return [];
-    if (tiles.length > 1 && tiles[0].field) {
+    if (!alignImages || alignImages.length === 0) return [];
+    if (alignImages.length > 1 && alignImages[0].field) {
       if (sortOrder === SortOrder.ascending) {
-        sortedTiles = sorted.sort(
+        sortedTiles = alignImages.sort(
           (a, b) =>
             Number(a.field.split('f')[1]) - Number(b.field.split('f')[1]),
         );
       } else
-        sortedTiles = sorted.sort(
+        sortedTiles = alignImages.sort(
           (a, b) =>
             Number(b.field.split('f')[1]) - Number(a.field.split('f')[1]),
         );
-    } else sortedTiles = sorted;
+    } else sortedTiles = alignImages;
 
     //console.log(sortedTiles);
 
     if (!dim) {
-      return sortedTiles;
+      return alignImages;
     }
 
     //console.log(sortedTiles);
@@ -715,17 +716,17 @@ const TabTiling = (props) => {
 
   // When the row is changed in alignment part
   const inputTilingRows = (event) => {
-    if (!tiles || tiles.length === 0) return;
+    if (!alignImages || alignImages.length === 0) return;
     let r = Number(event.target.value);
     if (r <= 0) r = 1;
     setAlignRow(event.target.value === '' ? '' : r);
-    if (tiles) {
-      if (tiles.length < r) {
+    if (alignImages) {
+      if (alignImages.length < r) {
         setAlignRow(tiles.length);
         setAlignCol(1);
-        setDim([tiles.length, 1]);
+        setDim([alignImages.length, 1]);
       } else {
-        let c = Math.ceil(tiles.length / r);
+        let c = Math.ceil(alignImages.length / r);
         setAlignCol(c);
         setDim([r, c]);
       }
@@ -734,18 +735,18 @@ const TabTiling = (props) => {
 
   // When the col is changed in alignment part
   const inputTilingCols = (event) => {
-    if (!tiles) return;
+    if (!alignImages) return;
     let c = Number(event.target.value);
     if (c <= 0) c = 1;
     setAlignCol(event.target.value === '' ? '' : c);
 
-    if (tiles) {
-      if (tiles.length < c) {
-        setAlignCol(tiles.length);
+    if (alignImages) {
+      if (alignImages.length < c) {
+        setAlignCol(alignImages.length);
         setAlignRow(1);
-        setDim([1, tiles.length]);
+        setDim([1, alignImages.length]);
       }
-      let r = Math.ceil(tiles.length / c);
+      let r = Math.ceil(alignImages.length / c);
       setAlignRow(r);
       setDim([r, c]);
     }
@@ -1380,42 +1381,6 @@ const TabTiling = (props) => {
         >
           {/*  Tiling Preview  */}
           <div style={{ flexDirection: 'column' }}>
-            {selectedIndex === 1 && (
-              <Paper
-                variant="outlined"
-                sx={{ height: '800px', width: '600px' }}
-              >
-                <TransformWrapper minScale={0.2}>
-                  <TransformComponent
-                    wrapperStyle={{ height: '800px', width: '600px' }}
-                  >
-                    <ImageList
-                      cols={alignCol}
-                      gap={alignGapX}
-                      padding={alignBorder}
-                      sx={{
-                        mb: 0,
-                        pointerEvents: 'none',
-                        userSelect: 'none',
-                        height: '100%',
-                        width: '100%',
-                      }}
-                    >
-                      {alignImages.map(({ _id, thumbnail, filename }) => (
-                        <ImageListItem key={_id}>
-                          <img
-                            src={thumbnail}
-                            alt={filename}
-                            style={{ width: 100, height: 'auto' }}
-                          />
-                        </ImageListItem>
-                      ))}
-                    </ImageList>
-                  </TransformComponent>
-                </TransformWrapper>
-              </Paper>
-            )}
-
             {selectedIndex === 0 && (
               <Paper
                 variant="outlined"
@@ -1446,6 +1411,42 @@ const TabTiling = (props) => {
                             />
                           </ImageListItem>
                         </>
+                      ))}
+                    </ImageList>
+                  </TransformComponent>
+                </TransformWrapper>
+              </Paper>
+            )}
+
+            {selectedIndex === 1 && (
+              <Paper
+                variant="outlined"
+                sx={{ height: '800px', width: '600px' }}
+              >
+                <TransformWrapper minScale={0.2}>
+                  <TransformComponent
+                    wrapperStyle={{ height: '800px', width: '600px' }}
+                  >
+                    <ImageList
+                      cols={alignCol}
+                      gap={alignGapX}
+                      padding={alignBorder}
+                      sx={{
+                        mb: 0,
+                        pointerEvents: 'none',
+                        userSelect: 'none',
+                        height: '100%',
+                        width: '100%',
+                      }}
+                    >
+                      {alignedImageList.map(({ _id, thumbnail, filename }) => (
+                        <ImageListItem key={_id}>
+                          <img
+                            src={thumbnail}
+                            alt={filename}
+                            style={{ width: 100, height: 'auto' }}
+                          />
+                        </ImageListItem>
                       ))}
                     </ImageList>
                   </TransformComponent>
